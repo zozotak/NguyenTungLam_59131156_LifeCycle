8c0cbc89b9b0a5c2c246ccd798c6c1c159b9b094
DELL <zozotak.boss@gmail.com>
2020-04-20 15:15:29 +0700

    V 1.0

--- app/src/main/java/vn/edu/ntu/tunglam/nguyentunglam_59131156_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/tunglam/nguyentunglam_59131156_lifecycle/MainActivity.java
@@ -3,12 +3,66 @@ package vn.edu.ntu.tunglam.nguyentunglam_59131156_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycle", "onCreate ");
+        Toast.makeText(getApplicationContext(), "This On Create", Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle", "onStart");
+        Toast.makeText(getApplicationContext(), "This On Start", Toast.LENGTH_SHORT).show();
+        time = findViewById(R.id.time);
+        SimpleDateFormat spF = new SimpleDateFormat("HH:mm:ss");
+        String date = spF.format(new Date());
+        time.setText(date);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle", "onRestart");
+        Toast.makeText(getApplicationContext(), "This On Restart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle", "onResume");
+        Toast.makeText(getApplicationContext(), "This On Resume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle", "onPause");
+        Toast.makeText(getApplicationContext(), "This On Pause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle", "onDestroy");
     }
 }
