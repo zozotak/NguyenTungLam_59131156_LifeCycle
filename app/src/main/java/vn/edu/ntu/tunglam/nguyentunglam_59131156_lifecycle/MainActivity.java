8c981c49ff2045969812295b3bc9654f8d11c4b0
DELL <zozotak.boss@gmail.com>
2020-04-20 10:11:58 +0700

    V 1.0

---
+++ app/src/main/java/vn/edu/ntu/tunglam/nguyentunglam_59131156_lifecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.edu.ntu.tunglam.nguyentunglam_59131156_lifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
